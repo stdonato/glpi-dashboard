929d4abfb8f9da0fe418ff90d6ecc3df712601fb
Stevenes Donato <stevenesdonato@gmail.com>
2019-06-24 10:23:16 -0400

    Update metrics.inc.php

--- front/metrics/metrics.inc.php
+++ front/metrics/metrics.inc.php
@@ -302,7 +302,7 @@ $res_days = 0;
 // SLA time
 $sql_cham = 
 "SELECT glpi_tickets.id AS id, glpi_tickets.name AS descr, glpi_tickets.date AS date, glpi_tickets.solvedate as solvedate, 
-glpi_tickets.status, glpi_tickets.due_date AS duedate, sla_waiting_duration AS slawait, glpi_tickets.type,
+glpi_tickets.status, glpi_tickets.time_to_resolve AS duedate, sla_waiting_duration AS slawait, glpi_tickets.type,
 FROM_UNIXTIME( UNIX_TIMESTAMP( `glpi_tickets`.`solvedate` ) , '%Y-%m' ) AS date_unix, AVG( glpi_tickets.solve_delay_stat ) AS time
 FROM glpi_tickets
 WHERE glpi_tickets.is_deleted = 0
