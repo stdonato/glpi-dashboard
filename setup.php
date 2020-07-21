8afb284673c83c3c08438a2ffdb62f311f398945
Stevenes Donato <stevenesdonato@gmail.com>
2020-07-21 17:49:27 -0400

    Update setup.php

--- setup.php
+++ setup.php
@@ -21,7 +21,7 @@ function plugin_version_dashboard(){
 	global $DB, $LANG;
 
 	return array('name'			=> __('Dashboard','dashboard'),
-					'version' 			=> '0.9.8',
+					'version' 			=> '0.9.9',
 					'author'			   => '<a href="https://forge.glpi-project.org/projects/dashboard"> Stevenes Donato </b> </a>',
 					'license'		 	=> 'GPLv2+',
 					'homepage'			=> 'https://forge.glpi-project.org/projects/dashboard',
