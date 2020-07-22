3250458d76ad103f05f117a77d2a5783b7550cb1
stdonato <stevenesdonato@gmail.com>
2020-07-22 14:06:40 -0400

    1.0.0-alpha

--- setup.php
+++ setup.php
@@ -21,7 +21,7 @@ function plugin_version_dashboard(){
 	global $DB, $LANG;
 
 	return array('name'			=> __('Dashboard','dashboard'),
-					'version' 			=> '0.9.9',
+					'version' 			=> '1.0.0',
 					'author'			   => '<a href="https://forge.glpi-project.org/projects/dashboard"> Stevenes Donato </b> </a>',
 					'license'		 	=> 'GPLv2+',
 					'homepage'			=> 'https://forge.glpi-project.org/projects/dashboard',
